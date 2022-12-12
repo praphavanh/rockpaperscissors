a2c09fdc8b501330955079d07c5c7e03d203fa56
Zakk <praphavanh@gmail.com>
2022-12-12 16:41:32 -0600

    cloned repo, created index and main with pseudocode

---
+++ main.js
@@ -0,0 +1,12 @@
+// 1. make a function that will randomly return rock paper or scissors.
+// 2. function that does a single round of Rock paper scissors 
+//3. two parameters of playerselection (case insensitive) and computerselection
+//4.  return a string that declares winner of a round
+
+//function gCC random choice between rps
+// function to loop only once
+//ask for user input of rps, case insenstive
+//set win/lose parameters compared to computer choice
+//return win loss to user between input and random choice 
+
+
