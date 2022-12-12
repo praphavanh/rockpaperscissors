d6d8e3367e72084efb0e3db8773a63675540608c
Zakk <praphavanh@gmail.com>
2022-12-12 17:04:14 -0600

    base functions added

--- main.js
+++ main.js
@@ -9,4 +9,16 @@
 //set win/lose parameters compared to computer choice
 //return win loss to user between input and random choice 
 
+const computerChoicesArray = ["rock", "paper", "scissors"]; //create array with 3 choice pool 
 
+function getComputerChoice () { //creating function name
+    return computerChoicesArray[Math.floor(Math.random() * computerChoicesArray.length)]; //random is 0 to x.9999. math.Floor rounds decimal down to choose what object in the array. .length tells what number to multiply by length of array
+}
+
+function playRound () {
+
+}
+
+function playerChoice() {
+    
+}
