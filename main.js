a3016f948f6fbfd4450a78de32c307f3f13e6323
Zakk <praphavanh@gmail.com>
2022-12-15 20:27:19 -0600

    functions and reutns fix to getcompchoice

--- main.js
+++ main.js
@@ -9,36 +9,66 @@
 //set win/lose parameters compared to computer choice
 //return win loss to user between input and random choice 
 //return computerChoicesArray[Math.floor(Math.random() * computerChoicesArray.length)]; //random is 0 to x.9999. math.Floor rounds decimal down to choose what object in the array. .length tells what number to multiply by length of array
-const computerChoicesArray = ["rock", "paper", "scissors"]; //create array with 3 choice pool 
+const computerOptionsArray = ["rock", "paper", "scissors"]; //create array with 3 choice pool 
+const playerSelection = "paper";
 
 function getComputerChoice () { //creating function name
-    const computerChoice = computerChoicesArray[Math.floor(Math.random() * computerChoicesArray.length)]; //computerChoicesArray[] brackets since its array
-    console.log(computerChoice);
+    const computerChoice = computerOptionsArray[Math.floor(Math.random() * computerOptionsArray.length)]; //computerChoicesArray[] brcomputerSelectionackets since its array
+    return computerChoice //return so computerChoice has a value outside of the function
+    console.log(`Computer Chose ${computerSelection}`);
 }//(Math.random() * computerChoicesArray.length) in parenthesis for operation precedence(i think)
 // math.random function returns random number between 0 and 1. 
 // * computerChoicesArray.length mulitplies the random by the array length
 
-function checkWinner(playerSelection, computerSelection) {
-    if(playerSelection === computerSelection) {
+const computerSelection = getComputerChoice();
+
+function checkWinner(playerSelection, computerSelection){
+    if(playerSelection == computerSelection) {
         return "Tie";
     }
     else if(
-    (playerSelection == "rock" && computerSelection == "scissors") ||
-    (playerSelection == "scissors" && computerSelection == "paper") ||
-    (playerSelection == "paper" && computerSelection == "rock")
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")
     ){
         return "Player";
     }
     else{
         return "Computer";
     }
-
-
-
-function playRound(playerSelection, computerSelection){
-
 }
 
-getComputerChoice();
+function suggestChoice(){
+    if (computerSelection == "scissors"){
+        return "rock";
+    }
+    else if (computerSelection == "paper"){
+        return "scissors";
+    }
+    else {
+        return "paper";
+    }
+}
+
+function playRound(playerSelection, computerSelection){
+    const result = checkWinner(playerSelection, computerSelection);
+    if(result == "Tie"){
+        return `It's a Tie! ${playerSelection} was on their mind too.`;
+    }
+    else if(result == "Player"){
+        return `Player Won! ${playerSelection} beats ${computerSelection}`;  
+    }   
+    else {
+        return `Computer won with ${computerSelection}... maybe choose ${suggestChoiceSelection} next time instead of 
+        ${playerSelection}.`;
+    }
+}
+
+//testing
+
+
+const suggestChoiceSelection = suggestChoice();
+console.log(`Computer Chose ${computerSelection}`)
+console.log(playRound(playerSelection, computerSelection));
 
 
